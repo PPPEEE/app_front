3586f058ab2f84affb0fc846853885870a839879
shen <357789504@qq.com>
2018-09-07 10:35:02 +0800

    add scanQR

--- src/component/scanCode.js
+++ src/component/scanCode.js
@@ -5,19 +5,71 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  SectionList,
+  TouchableOpacity,
   Text,
   Image,
   SafeAreaView
 } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RNCamera } from 'react-native-camera';
 
 export default class scanCode extends Component {
+
+  getQR(code){
+    alert(JSON.stringify(code));
+  }
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>scanCodeScreen!</Text>
+      <View style={styles.container}>
+        <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+            onBarCodeRead={this.getQR}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+        <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style = {styles.capture}
+        >
+            <Text style={{fontSize: 14}}> SNAP </Text>
+        </TouchableOpacity>
+        </View>
       </View>
     );
   }
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      console.log(data.uri);
+    }
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
+});
