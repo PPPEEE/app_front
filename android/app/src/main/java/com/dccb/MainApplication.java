b5585cd363a79095398d692a08e36f32f46d39e0
shen <357789504@qq.com>
2018-09-09 00:01:20 +0800

    del code new RNCameraPackage

--- android/app/src/main/java/com/dccb/MainApplication.java
+++ android/app/src/main/java/com/dccb/MainApplication.java
@@ -26,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraPackage(),
           new RNCameraPackage(),
           new PickerPackage(),
           new VectorIconsPackage()
